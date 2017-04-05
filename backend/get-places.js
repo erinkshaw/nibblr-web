2667f22866c87ec047d4ad0f698a988794e69271
Erin Shaw <erin.kimberly.shaw@gmail.com>
2017-04-04 20:52:38 -0400

    removed API key from backend

--- backend/get-places.js
+++ backend/get-places.js
@@ -1,20 +1,18 @@
 var request = require('sync-request')
 var fs = require('fs')
 
-var makeGooglePlacesSearchURL = function(location, radius, type){
+var makeGooglePlacesSearchURL = function(location, radius, type, key){
     var baseURL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/';
-    var key = 'AIzaSyDkBPnoUOVRR0ZLfP3pYupulojjTZa6NMc';
     var output = 'json';
     var fullURL = baseURL + output + '?' + 'key=' + key + '&' + 'location=' + location + '&' + 'radius=' + radius + '&' + 'type=' + type  
     return fullURL
 }
 
-var makeGooglePlacesPhotoURL = function(photoReference){
+var makeGooglePlacesPhotoURL = function(photoReference, key){
     var baseURL = 'https://maps.googleapis.com/maps/api/place/photo?';
-    var key = 'foobar';
     var maxHeight = 400;
     var fullURL = baseURL + 'key=' + key + '&' + 'maxheight=' + maxHeight + '&' + 'photoreference=' + photoReference;
-    return fullURL
+    return fullURL;
 }
 
 var callAPI = function(url) {
@@ -34,16 +32,20 @@ var getPlaceAttribute = function (response, attributeGetter) {
     return response.results.map(attributeGetter);
 }
 
-// Get photo references from Google Search API
+// Download restaurant pictures in an area around lat/lon.
+var key = JSON.parse(fs.readFileSync('key.json', 'utf8'))['key'];
 var latLon = process.argv[2] + ',' + process.argv[3]   // 40.684478,-73.941467
-var searchUrl = makeGooglePlacesSearchURL(latLon, '1000', 'restaurant');
+
 console.log('Pinging Google Places Search API')
+var searchUrl = makeGooglePlacesSearchURL(latLon, '1000', 'restaurant', key);
 var places = getJSONFromAPI(searchUrl)['results'];
+
 console.log('Found restaraunts near ' + latLon)
 places.forEach(function(place) {
     var name =  place['name'];
     var photoReference = place['photos'][0]['photo_reference'];
-    var photoURL = makeGooglePlacesPhotoURL(photoReference);
+    var photoURL = makeGooglePlacesPhotoURL(photoReference, key);
+
     console.log('Downloading picture for ' + name)
     var responseBody = callAPI(photoURL);
     fs.writeFile("img/" + name + ".png", responseBody, function(err){console.log('Saved picture for ' + name);});
